3fe561b5bb577ba069647461337c06ba655a1018
Koy Saeteurn <kosatun@aol.com>
2018-08-02 20:13:41 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -5,7 +5,7 @@ class Backer
     @backed_projects = []
   end 
   
-  def back_project(project)
+  def back_project(project, backer)
     @backed_projects << project 
     @backers << backer
   end   
