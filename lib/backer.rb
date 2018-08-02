a837e2f7c9d91a48f41beb914331bad6f5611ee5
Koy Saeteurn <kosatun@aol.com>
2018-08-02 20:15:24 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,13 @@
 class Backer 
-  attr_accessor :name, :backed_projects, :backers
+  attr_accessor :name, :backed_projects
   def initialize(name) 
     @name = name 
     @backed_projects = []
   end 
   
-  def back_project(project, backer)
+  def back_project(project)
     @backed_projects << project 
-    @backers << backer
+    @backers << self 
   end   
     
     
